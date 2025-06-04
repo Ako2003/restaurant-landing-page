72aa01851c9957f13acf22ed9eca068539c130de
Ako2003 <akifmursalov@gmail.com>
2025-06-04 13:20:16 +0400

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,29 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const helvetica = localFont({
+    src: [
+        {
+            path: "../../public/fonts/helvetica/Helvetica.ttf",
+        },
+        {
+            path: "../../public/fonts/helvetica/Helvetica-Bold.ttf",
+        },
+        {
+            path: "../../public/fonts/helvetica/Helvetica-BoldOblique.ttf",
+        },
+        {
+            path: "../../public/fonts/helvetica/Helvetica-Light.ttf",
+        },
+        {
+            path: "../../public/fonts/helvetica/Helvetica-Oblique.ttf",
+        },
+    ],
+    display: 'swap',
+    style: 'normal',
+    variable: '--font-helvetica'
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +38,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${helvetica.variable} antialiased`}
       >
         {children}
       </body>
